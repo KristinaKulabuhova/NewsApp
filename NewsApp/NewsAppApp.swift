b172b3e37071b193fc87d82bfec22ef1493d302c
Kristina Kulabukhova <kulabukhova.ka@phystech.edu>
2023-12-14 12:51:10 +0300

    add homeScreen

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Kristina on 10.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
